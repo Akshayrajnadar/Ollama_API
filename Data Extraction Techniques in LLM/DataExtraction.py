5a93393c5ea4308e60d7822047e0dcd2b91c487c
Akshayraj Nadar <akshayraj08032000@gmail.com>
2025-01-07 15:04:34 +0530

    Data Extraction Methods

---
+++ Data Extraction Techniques in LLM/DataExtraction.py
@@ -0,0 +1,39 @@
+# Extract from a text file
+# from langchain_community.document_loaders import TextLoader
+
+# loader = TextLoader("speech.txt")
+# text_loader = loader.load()
+# text_loader
+
+# # Extract from web page
+# from langchain_community.document_loaders import WebBaseLoader
+# import bs4
+
+# loader = WebBaseLoader(
+#     web_paths= ("https://en.wikipedia.org/wiki/Large_language_model",),
+#     bs_kwargs=dict(parse_only=bs4.SoupStrainer(
+#     class_ = ("mw-content-container","mw-page-container-inner")
+# )))
+
+# web_page_loader = loader.load()
+# web_page_loader
+
+# Extract data from a PDF file
+from langchain_community.document_loaders import PyPDFLoader
+
+loader = PyPDFLoader("Data Extraction Techniques in LLM/attention.pdf")
+pdf_loader = loader.load()
+print(pdf_loader)
+
+
+# Split data in to chunks
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,
+    chunk_overlap=200
+)
+
+documents = text_splitter.split_documents(pdf_loader)
+print(documents[0])
+
